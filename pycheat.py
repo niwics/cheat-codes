635a7b764b072e40d2957e075cec4d57c00b699f
Miroslav Kvasnica <miroslav.kvasnica@firma.seznam.cz>
2016-11-03 08:19:20 +0100

    feat: printing, dates, regexps, multiprocessing

--- pycheat.py
+++ pycheat.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 """
 How to use:
@@ -30,6 +31,49 @@ def datatypes():
     isinstance(my_variable, numbers.Number)
 
 
+def printing():
+    value1 = "val1"
+    value2 = "val2"
+    # python3 style
+    print("This is {0} and {1}".format(value1, value2))
+
+    # print to stderr
+    #print("Error while parsing the config file {0}".format(value1), file=sys.stderr)    # python3
+    print >> sys.stderr, "Error while parsing the config file {0}".format(value1)    # python 2
+
+    # print with leading zeros
+    for i in range(15):
+        print "%02d" % i
+
+def regexps():
+    # Regexps
+    # https://docs.python.org/2/library/re.html
+
+    # important thig when working with regexp patterns: do not forget to use
+    # raw strings = r"xyz"
+    import re
+
+    # basic searching
+    match = re.search(r"ne+dle", "searching neeedle in the haystack!")
+    print match.group(0)    # neeedle
+
+    # precompiling + group matching
+    regexp = re.compile(r"(\d+):\d+")
+    match = regexp.search("arrival in 12:00")
+    print "arrival hour: %s" % match.group(1)
+
+    # simple replacing
+    print re.sub(r"@", r"(at)", "me@email.com")   # me(at)email.com
+
+def dates():
+    # print month with leading zeros
+    import datetime
+    today_dt = datetime.datetime.now() # type datetime
+    today_date = date.today()   # type date
+    today_date2 = today_dt.date()
+    print "This is month: %s" % today_dt.strftime("%-m")   # alternative in print: "%02d"
+
+
 def generate_date_ranges(start_date_str, end_date_str):
     """
     In:
@@ -38,10 +82,8 @@ def generate_date_ranges(start_date_str, end_date_str):
     import datetime
     from dateutil.rrule import rrule, DAILY
 
-    # working with datet
+    # working with datetime
     #https://docs.python.org/2/library/datetime.html
-
-
 
 
     start = datetime.datetime.strptime(start_date_str, "%Y-%m-%d")
@@ -50,6 +92,40 @@ def generate_date_ranges(start_date_str, end_date_str):
     for dt in rrule(DAILY, dtstart=start, until=end):
         print dt.strftime("%Y-%m-%d")
 
+def multiprocessing_worker(worker_id, sleep_seconds):
+    """
+    Worker function for multiprocesing
+    """
+    print "Worker #%s started" % worker_id
+    import time
+    time.sleep(sleep_seconds)
+    msg = "Worker #%s finished" % worker_id
+    print msg
+    return msg
+
+def multiprocessing_pool_of_workers():
+    """
+    Multiprocessing Pool.
+    """
+    from multiprocessing import Pool
+    num_workers = 4
+    pool = Pool(num_workers)
+    workers_params = [[worker_id, num_workers-worker_id] for worker_id in range(num_workers)]
+    results = pool.map(multiprocessing_worker, workers_params)
+    print results
+
+def multiprocessing_process():
+    """
+    Multiprocessing Pool: https://pymotw.com/2/multiprocessing/basics.html
+    """
+    from multiprocessing import Process
+    num_workers = 4
+    jobs = []
+    for worker_id in range(num_workers):
+        p = Process(target=multiprocessing_worker, args=(worker_id, num_workers))
+        jobs.append(p)
+        p.start()
+
 def random():
     import random
     random.random()        # Random float x, 0.0 <= x < 1.0
@@ -57,6 +133,7 @@ def random():
     random.randint(1, 10)  # Integer from 1 to 10, endpoints included
     random.randrange(0, 101, 2)  # Even integer from 0 to 100
     random.choice('abcdefghij')  # Choose a random element
+    random.choice(['one', 'two', 'three'])  # Choose a random element
     random.choice(string.ascii_lowercase)   # random ASCII lowercase character
     random.shuffle([1, 2, 3, 4])    # shuffle the array
     random.shuffle([1, 2, 3, 4], 2)    # pick two random numbers
@@ -106,18 +183,42 @@ def main():
     # argparse example
     import argparse
     parser = argparse.ArgumentParser()
+    # group of exclusive options
+    sp = parser.add_subparsers()
+    sp.add_parser('start', help='Starts %(prog)s daemon')
+    sp.add_parser('stop', help='Stops %(prog)s daemon')
+    sp.add_parser('restart', help='Restarts %(prog)s daemon')
+    # required
     parser.add_argument('--host', help='Hostname help text', required=True)
+    # default
     parser.add_argument('--port', help='some help text', default=8080)
+    # options (possibilities)
+    parser.add_argument('--protocol', help='some help text', choices=['http', 'https'])
+    # type setting
     parser.add_argument('--workers', help='Number of parallel workers', type=int, default=1)
-    args = parser.parse_args()
+    # bool value (store_true set True and it omits the value; similar is store_false and store_const)
+    parser.add_argument('--dry-run', help='This is bool flag', action='store_true')
+    args = parser.parse_args()  # accessing: args.myparam
+    # you cannot use reserved keywords like "from" (ex. args.from) - in that case you need a dict:
+    # source: https://parezcoydigo.wordpress.com/2012/08/04/from-argparse-to-dictionary-in-python-2-7/
+    opts = vars(args)           # accessing: opts['myparam']
     # in case of invalid input, it prints the help and exits
     if len(USER_IDS) == len(args.entities):
         print "WARNING: Number of users should not be the multiple of number of entities (because of caching)!"
 
+    # return codes
     import sys
     import os
-    sys.exit(os.EX_CONFIG)
+    # useful return codes: (more on https://docs.python.org/2/library/os.html)
+    # EX_OK
+    # EX_USAGE - bad program usage
+    # EX_DATAERR - incorrect input data
+    # EX_UNAVAILABLE - required service is unavailable
+    # EX_SOFTWARE - software error
+    # EX_CONFIG - configuration error
+    # EX_NOTFOUND
     sys.exit(os.EX_OK)
+
 
 
 if __name__ == "__main__":
